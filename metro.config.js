cfa6d52946c045c7b87a8016fcc52c9c0c45a938
shadolite <shadolite@proton.me>
2025-10-27 13:56:32 -0600

    Added expo-sqlite

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,19 @@ const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Add wasm asset support
+config.resolver.assetExts.push('wasm');
+ 
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    middleware(req, res, next);
+  };
+};
+ 
 //TODO: This bypasses import.meta checks: https://github.com/expo/expo/issues/30323
 config.resolver.unstable_conditionNames = ['browser', 'require', 'react-native'];
 
