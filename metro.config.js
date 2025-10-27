f5c74f3a211a3424c363ac6332b502458287dd3e
shadolite <shadolite@proton.me>
2025-10-27 13:29:02 -0600

    Fixed build issues

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+//TODO: This bypasses import.meta checks: https://github.com/expo/expo/issues/30323
+config.resolver.unstable_conditionNames = ['browser', 'require', 'react-native'];
 
 module.exports = withNativeWind(config, { input: './global.css' })
